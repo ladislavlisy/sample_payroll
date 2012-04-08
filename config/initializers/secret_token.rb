fb71452ca5a4a37af145914c86dce84ea18ffb41
ladislavlisy <ladislav.lisy@seznam.cz>
2012-04-08 14:20:16 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamplePayroll::Application.config.secret_token = '5c1c1b551b2b6be10474964a429d1cc4f7ca97ccc670e162d389f06f5f5bb8430a60d09c08d6926b4ba50c039af40bf984409d9bd98c2dd449923dadf6ab8d56'
